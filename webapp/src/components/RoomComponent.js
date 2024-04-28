b5b6e24666b3e56544d6757f2b3e2ebc64bb7392
Dmitry Kolobaev <kol-di@yandex.ru>
2024-04-28 22:55:04 +0300

    room styles

--- webapp/src/components/RoomComponent.js
+++ webapp/src/components/RoomComponent.js
@@ -1,27 +1,41 @@
-import React, { useEffect } from 'react';
-import styles from './RoomComponent.module.css'; // Import CSS module (create this file next)
+// RoomComponent.js
+import React, { useState } from 'react';
+import styles from './RoomComponent.module.css';
 
 const RoomComponent = ({ roomData }) => {
+    const [searchActive, setSearchActive] = useState(false);
+
+    const handleSearchFocus = () => {
+        setSearchActive(true);
+    };
+
+    const handleSearchBlur = () => {
+        setSearchActive(false);
+    };
+
     return (
         <div className={styles.container}>
-            <header className={styles.header}>
+            <div className={styles.roomNameContainer}>
                 <h1 className={styles.roomName}>{roomData.roomName}</h1>
-                <input type="text" placeholder="Search tracks..." className={styles.searchInput} />
-            </header>
-            <ul className={styles.adminList}>
-                {roomData.admins.map((admin, index) => (
-                    <li key={index} className={styles.admin}>{admin}</li> // Display telegramId or other identifier
-                ))}
-            </ul>
-            <div className={styles.trackListContainer}>
-                <h2 className={styles.tracksTitle}>Tracks</h2>
-                <ul className={styles.trackList}>
-                    {roomData.tracks.map((track, index) => (
-                        <li key={index} className={styles.track}>
-                            Spotify ID: {track.spotifyId} - Votes: {track.votes}
-                        </li>
-                    ))}
-                </ul>
+            </div>
+            <div className={styles.subLayer}>
+                <input
+                    type="text"
+                    placeholder="Search tracks..."
+                    className={`${styles.searchInput} ${styles.stickySearch}`}
+                    onFocus={handleSearchFocus}
+                    onBlur={handleSearchBlur}
+                    style={{ backgroundColor: searchActive ? '#fff' : '#eee' }}
+                />
+                <div className={styles.trackListContainer}>
+                    <ul className={styles.trackList}>
+                        {roomData.tracks.map((track, index) => (
+                            <li key={index} className={styles.track}>
+                                Spotify ID: {track.spotifyId} - Votes: {track.votes}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
